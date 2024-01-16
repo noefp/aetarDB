c617d3800e895080ffbccd8ae504379f3becb9f6
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-01-16 10:39:56 +0100

    seedling dataset images and description

---
+++ egdb_custom_text/expr_datasets/07_seedlings.php
@@ -0,0 +1,42 @@
+<p>
+  RNA-seq of <i>Aethionema arabicum</i> (Turkey ecotype) germinating seeds, germinated seeds and early seedlings derived from natural M+ seed and IND fruit diaspores and bare M- seed (through pericarp removal). Germinating seeds imbibed at 9°C were sampled at the physiological stages of 1% germination (T1%) and 100% germination (T100%). Root and shoot tissues of seedlings were sampled 72 hours and 240 hours after post-germinative growth at 30°C (A). 
+</p>
+<h4>Experimental Conditions (3 replicates):</h4>
+
+<div style="width:100%">
+  
+  <img class='rounded' src='<?php echo "$images_path/seedling_desc.png";?>' style="max-width:900px">
+  
+  <table class="table" style="max-width:900px">
+    <tr><th>Sample_ID</th><th>Tissue type</th><th>Morphotype</th><th>Physiological time</th></tr>
+    <tr><td>D_T20pSseed9C52h</td><td>seed</td><td>M+</td><td>t1%</td></tr>
+    <tr><td>D_T20mSseed9C53h</td><td>seed</td><td>M-</td><td>t1%</td></tr>
+    <tr><td>D_T20iSseed9C100h</td><td>seed</td><td>IND</td><td>t1%</td></tr>
+    <tr><td>D_T20pSseed9C68h</td><td>seed</td><td>M+</td><td>t100%</td></tr>
+    <tr><td>D_T20mSseed9C72h</td><td>seed</td><td>M-</td><td>t100%</td></tr>
+    <tr><td>D_T20iSseed9C200h</td><td>seed</td><td>IND</td><td>t100%</td></tr>
+    <tr><td>D_T20pSroot30C72h</td><td>root</td><td>M+</td><td>72 HAT</td></tr>
+    <tr><td>D_T20mSroot30C72h</td><td>root</td><td>M-</td><td>72 HAT</td></tr>
+    <tr><td>D_T20iSroot30C72h</td><td>root</td><td>IND</td><td>72 HAT</td></tr>
+    <tr><td>D_T20pSroot30C240h</td><td>root</td><td>M+</td><td>240 HAT</td></tr>
+    <tr><td>D_T20mSroot30C240h</td><td>root</td><td>M-</td><td>240 HAT</td></tr>
+    <tr><td>D_T20iSroot30C240h</td><td>root</td><td>IND </td><td>240 HAT</td></tr>
+    <tr><td>D_T20pSshoot30C72h</td><td>shoot</td><td>M+</td><td>72 HAT</td></tr>
+    <tr><td>D_T20mSshoot30C72h</td><td>shoot</td><td>M-</td><td>72 HAT</td></tr>
+    <tr><td>D_T20iSshoot30C72h</td><td>shoot</td><td>IND</td><td>72 HAT</td></tr>
+    <tr><td>D_T20pSshoot30C240h</td><td>shoot</td><td>M+</td><td>240 HAT</td></tr>
+    <tr><td>D_T20mSshoot30C240h</td><td>shoot</td><td>M-</td><td>240 HAT</td></tr>
+    <tr><td>D_T20iSshoot30C240h</td><td>shoot</td><td>IND</td><td>240 HAT</td></tr>
+  </table>
+</div>
+
+
+
+
+
+<p>Sample nomenclature starts with D, followed by the genotype (T, Turkey), paternal temperature (20), origin diaspore morphotype (p, m or i for M+, M- and IND, respectively), tissue type (seed, root or shoot) and timepoint in hours (e.g. 52h). 
+</p>
+
+<p>
+  Raw data can be found in the BioProject <a href="https://www.ncbi.nlm.nih.gov/sra/PRJNA639399" target="_blank">PRJNA639399</a>.
+</p>
